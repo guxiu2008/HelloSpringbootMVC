3fffd7f89a0a6381cb2489a8fdd0852eb31024ca
guxiu2008 <513201818@qq.com>
2020-03-25 22:37:16 +0800

    Update environment management

--- src/main/java/com/guxiu2008/hellospringbootmvc/controller/HelloController.java
+++ src/main/java/com/guxiu2008/hellospringbootmvc/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.guxiu2008.hellospringbootmvc.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Package: com.guxiu2008.hellospringbootmvc
@@ -17,8 +19,17 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/")
 public class HelloController {
 
+    @Value("${env.url}")
+    private String envUrl;
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String readersBooks(@PathVariable("id") Integer id, Model model) {
         return "a/Helloworld";
+    }
+
+    @RequestMapping("getEnvUrl")
+    @ResponseBody
+    public String getEnvUrl() {
+        return envUrl;
     }
 }
--- src/main/resources/static/js/Helloallan1.js
+++ src/main/resources/static/js/Helloallan1.js
@@ -1,3 +1,17 @@
 HelloAllan = function () {
     alert("Hello Allan!");
 }
+
+getEnvUrl = function () {
+    $.ajax({
+        type: "get",
+        url: "getEnvUrl",
+        success: function (data)
+        {
+            alert(data);
+        },
+        error:function (XMLHttpRequest, textStatus, errorThrown) {
+            alert("«Î«Û ß∞‹£°");
+        }
+    });
+}
